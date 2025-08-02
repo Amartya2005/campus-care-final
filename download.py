e5e1cb8c092275ad61913cfc67823b054d7fd541
Amartya2005 <sarangeeamartya@gmail.com>
2025-08-03 00:53:19 +0530

    Final Fix: Add explicit NLTK download script

---
+++ download.py
@@ -0,0 +1,7 @@
+import nltk
+
+print("Downloading necessary NLTK corpora...")
+nltk.download('punkt')
+nltk.download('brown')
+nltk.download('averaged_perceptron_tagger')
+print("All NLTK downloads complete.")
